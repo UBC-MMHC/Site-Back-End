21b61f1dcd688050fd98bdcb3ca76fbd81ed9f42
Jagmans <jagmansidhu1@icloud.com>
2025-10-29 17:55:26 -0700

    Implemented user auth with google

--- src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Config/SecurityConfig.java
+++ src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Config/SecurityConfig.java
@@ -1,4 +1,82 @@
 package com.ubcmmhcsoftware.ubcmmhc_web.Config;
 
+import com.ubcmmhcsoftware.ubcmmhc_web.Entity.User;
+import com.ubcmmhcsoftware.ubcmmhc_web.Repository.UserRepository;
+import com.ubcmmhcsoftware.ubcmmhc_web.Service.JWTService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+    private final JWTService jwtService;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationSuccessHandler successHandler) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oauth2UserService())
+                        )
+                        .successHandler(successHandler)
+                );
+
+
+        return http.build();
+    }
+
+    // Adds user to database if not already there
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        return request -> {
+            OAuth2User oauthUser = new DefaultOAuth2UserService().loadUser(request);
+
+            String email = oauthUser.getAttribute("email");
+            userRepository.findByEmail(email).orElseGet(() -> {
+                User user = new User();
+                user.setGoogleId(oauthUser.getAttribute("sub"));
+                user.setEmail(email);
+                user.setName(oauthUser.getAttribute("name"));
+                user.setRole("USER");
+                return userRepository.save(user);
+            });
+
+            return oauthUser;
+        };
+    }
+
+    // Test Login using http://localhost:8080/oauth2/authorization/google after login redirect to localhost:3000/....
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return (request, response, authentication) -> {
+            OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+            String email = oauthUser.getAttribute("email");
+
+            String token = jwtService.generateToken(email);
+
+            response.setStatus(HttpServletResponse.SC_FOUND);
+            response.setHeader("Authorization", "Bearer " + token);
+            response.sendRedirect("http://localhost:3000?token=" + token);
+        };
+    }
+
 }
---
+++ src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Service/JWTService.java
@@ -0,0 +1,51 @@
+package com.ubcmmhcsoftware.ubcmmhc_web.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTService {
+    private static final int secondsToAdd = 60 * 60 * 24 * 24;
+    private static SecretKey key;
+
+    public JWTService(@Value("${spring.jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
+        Instant now = Instant.now();
+
+        Instant expiry = now.plusSeconds(secondsToAdd);
+
+        return Jwts.builder()
+                .header()
+                .type("JWT")
+                .and()
+                .claims()
+                .subject(email)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(expiry))
+                .and()
+                .signWith(key, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claimsResolver.apply(claims);
+    }
+}
