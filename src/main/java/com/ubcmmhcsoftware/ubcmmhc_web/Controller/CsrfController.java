123146831e02d8fb914d4de0fe2d2817cfb3c32c
Jagmans <jagmansidhu1@icloud.com>
2025-11-29 19:57:02 -0800

    Minor csrf changes

--- src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Controller/CsrfController.java
+++ src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Controller/CsrfController.java
@@ -4,11 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class CsrfController {
 
-    @GetMapping
+    @GetMapping("/csrf-token")
     public ResponseEntity<?> getCsrfToken() {
         return ResponseEntity.status(HttpStatus.OK).build();
 
---
+++ src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/DTO/ForgotPasswordDTO.java
@@ -0,0 +1,8 @@
+package com.ubcmmhcsoftware.ubcmmhc_web.DTO;
+
+import lombok.Data;
+
+@Data
+public class ForgotPasswordDTO {
+    private String email;
+}
