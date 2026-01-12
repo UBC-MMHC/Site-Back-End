febaf0860dde21fc5677316225a9f96248592741
Jagmans <jagmansidhu1@icloud.com>
2026-01-11 21:32:44 -0800

    Membership prices differences

---
+++ src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Enum/MembershipType.java
@@ -0,0 +1,10 @@
+package com.ubcmmhcsoftware.ubcmmhc_web.Enum;
+
+/**
+ * Represents the different types of memberships available.
+ */
+public enum MembershipType {
+    UBC_STUDENT,
+    NON_UBC_STUDENT,
+    NON_STUDENT
+}
