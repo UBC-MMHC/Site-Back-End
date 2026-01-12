6cfbc95a06fe2ee327d1674302245634529d77fd
StashC <stashubc@student.ubc.ca>
2026-01-12 10:10:08 -0800

    make newsletter signup add to list

--- src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Service/NewsletterService.java
+++ src/main/java/com/ubcmmhcsoftware/ubcmmhc_web/Service/NewsletterService.java
@@ -2,11 +2,9 @@ package com.ubcmmhcsoftware.ubcmmhc_web.Service;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import com.ubcmmhcsoftware.ubcmmhc_web.Entity.NewsletterSubscriber;
-import com.ubcmmhcsoftware.ubcmmhc_web.Repository.NewsletterRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,9 +13,12 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
-import com.ubcmmhcsoftware.ubcmmhc_web.Entity.User;
 import org.springframework.web.server.ResponseStatusException;
+
+import com.ubcmmhcsoftware.ubcmmhc_web.Entity.NewsletterSubscriber;
+import com.ubcmmhcsoftware.ubcmmhc_web.Repository.NewsletterRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -25,13 +26,12 @@ public class NewsletterService {
     private final NewsletterRepository newsletterRepository;
     @Value("${brevo.api-key}")
     private String brevoApiKey;
-    private static final String brevoBaseUrl = "https://api.brevo.com/v3";
+    @Value("${brevo.newsletter-list-id}")
+    private Integer newsletterListId;
+    @Value("${brevo.base-url}")
+    private final String brevoBaseUrl = "https://api.brevo.com/v3";
 
     public void addEmail(String email){
-        System.out.println("asdddd");
-        System.out.println(this.brevoApiKey);
-        System.out.println(email);
-
         // TODO add some validation this is actually an email
         NewsletterSubscriber existingSubscriber = this.newsletterRepository.findByEmail(email);
         if (existingSubscriber != null && !existingSubscriber.isUnsubscribed()){
@@ -47,7 +47,9 @@ public class NewsletterService {
         headers.set("api-key", brevoApiKey);
 
         Map<String, Object> body = new HashMap<>();
+
         body.put("email", email);
+        body.put("listIds", List.of(newsletterListId));
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
